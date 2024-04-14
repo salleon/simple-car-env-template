83ee760458fe0e3d06e32829576793593b96f55f
salleon <116154066+salleon@users.noreply.github.com>
2024-04-14 19:36:13 +1000

    q learning implemented
    edited to add q learning

--- simple_driving/envs/simple_driving_env.py
+++ simple_driving/envs/simple_driving_env.py
@@ -6,7 +6,6 @@ from pybullet_utils import bullet_client as bc
 from simple_driving.resources.car import Car
 from simple_driving.resources.plane import Plane
 from simple_driving.resources.goal import Goal
-import matplotlib.pyplot as plt
 import time
 
 RENDER_HEIGHT = 720
@@ -28,9 +27,9 @@ class SimpleDrivingEnv(gym.Env):
         self.np_random, _ = gym.utils.seeding.np_random()
 
         if renders:
-          self._p = bc.BulletClient(connection_mode=p.GUI)
+            self._p = bc.BulletClient(connection_mode=p.GUI)
         else:
-          self._p = bc.BulletClient()
+            self._p = bc.BulletClient()
 
         self.reached_goal = False
         self._timeStep = 0.01
@@ -47,6 +46,13 @@ class SimpleDrivingEnv(gym.Env):
         self.reset()
         self._envStepCounter = 0
 
+        # Q-learning parameters
+        self.Q = np.zeros((self.observation_space.shape[0], self.action_space.n))
+        self.alpha = 0.1  # learning rate
+        self.gamma = 0.99  # discount factor
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_decay = 0.99  # decay rate for epsilon
+
     def step(self, action):
         # Feed action to the car and get observation of car's state
         if (self._isDiscrete):
@@ -57,36 +63,39 @@ class SimpleDrivingEnv(gym.Env):
             action = [throttle, steering_angle]
         self.car.apply_action(action)
         for i in range(self._actionRepeat):
-          self._p.stepSimulation()
-          if self._renders:
-            time.sleep(self._timeStep)
+            self._p.stepSimulation()
+            if self._renders:
+                time.sleep(self._timeStep)
 
-          carpos, carorn = self._p.getBasePositionAndOrientation(self.car.car)
-          goalpos, goalorn = self._p.getBasePositionAndOrientation(self.goal_object.goal)
-          car_ob = self.getExtendedObservation()
+            carpos, _ = self._p.getBasePositionAndOrientation(self.car.car)
+            goalpos, _ = self._p.getBasePositionAndOrientation(self.goal_object.goal)
+            car_ob = self.getExtendedObservation()
 
-          if self._termination():
-            self.done = True
-            break
-          self._envStepCounter += 1
+            if self._termination():
+                self.done = True
+                break
+            self._envStepCounter += 1
 
         # Compute reward as L2 change in distance to goal
-        # dist_to_goal = math.sqrt(((car_ob[0] - self.goal[0]) ** 2 +
-                                  # (car_ob[1] - self.goal[1]) ** 2))
-        dist_to_goal = math.sqrt(((carpos[0] - goalpos[0]) ** 2 +
-                                  (carpos[1] - goalpos[1]) ** 2))
-        # reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
+        dist_to_goal = np.linalg.norm(carpos - goalpos)
         reward = -dist_to_goal
         self.prev_dist_to_goal = dist_to_goal
 
-        # Done by reaching goal
-        if dist_to_goal < 1.5 and not self.reached_goal:
-            #print("reached goal")
-            self.done = True
-            self.reached_goal = True
+        # Update Q-value for previous state and action
+        self.Q[self.prev_state, self.prev_action] += self.alpha * (
+                    reward + self.gamma * np.max(self.Q[car_ob]) - self.Q[self.prev_state, self.prev_action])
+
+        # Choose next action using epsilon-greedy policy
+        if np.random.rand() < self.epsilon:
+            action = self.action_space.sample()
+        else:
+            action = np.argmax(self.Q[car_ob])
+
+        self.prev_state = car_ob
+        self.prev_action = action
 
         ob = car_ob
-        return ob, reward, self.done, dict()
+        return ob, reward, self.done, {}
 
     def seed(self, seed=None):
         self.np_random, seed = gym.utils.seeding.np_random(seed)
@@ -106,7 +115,7 @@ class SimpleDrivingEnv(gym.Env):
              self.np_random.uniform(-9, -5))
         y = (self.np_random.uniform(5, 9) if self.np_random.integers(2) else
              self.np_random.uniform(-9, -5))
-        self.goal = (x, y)
+        self.goal = np.array([x, y])
         self.done = False
         self.reached_goal = False
 
@@ -114,76 +123,22 @@ class SimpleDrivingEnv(gym.Env):
         self.goal_object = Goal(self._p, self.goal)
 
         # Get observation to return
-        carpos = self.car.get_observation()
+        carpos, _ = self._p.getBasePositionAndOrientation(self.car.car)
+        self.prev_state = self.getExtendedObservation()
+        self.prev_action = self.action_space.sample()
 
-        self.prev_dist_to_goal = math.sqrt(((carpos[0] - self.goal[0]) ** 2 +
-                                           (carpos[1] - self.goal[1]) ** 2))
-        car_ob = self.getExtendedObservation()
+        self.prev_dist_to_goal = np.linalg.norm(carpos - self.goal)
+        car_ob = self.prev_state
         return np.array(car_ob, dtype=np.float32)
 
     def render(self, mode='human'):
-        if mode == "fp_camera":
-            # Base information
-            car_id = self.car.get_ids()
-            proj_matrix = self._p.computeProjectionMatrixFOV(fov=80, aspect=1,
-                                                       nearVal=0.01, farVal=100)
-            pos, ori = [list(l) for l in
-                        self._p.getBasePositionAndOrientation(car_id)]
-            pos[2] = 0.2
-
-            # Rotate camera direction
-            rot_mat = np.array(self._p.getMatrixFromQuaternion(ori)).reshape(3, 3)
-            camera_vec = np.matmul(rot_mat, [1, 0, 0])
-            up_vec = np.matmul(rot_mat, np.array([0, 0, 1]))
-            view_matrix = self._p.computeViewMatrix(pos, pos + camera_vec, up_vec)
-
-            # Display image
-            # frame = self._p.getCameraImage(100, 100, view_matrix, proj_matrix)[2]
-            # frame = np.reshape(frame, (100, 100, 4))
-            (_, _, px, _, _) = self._p.getCameraImage(width=RENDER_WIDTH,
-                                                      height=RENDER_HEIGHT,
-                                                      viewMatrix=view_matrix,
-                                                      projectionMatrix=proj_matrix,
-                                                      renderer=p.ER_BULLET_HARDWARE_OPENGL)
-            frame = np.array(px)
-            frame = frame[:, :, :3]
-            return frame
-            # self.rendered_img.set_data(frame)
-            # plt.draw()
-            # plt.pause(.00001)
-
-        elif mode == "tp_camera":
-            car_id = self.car.get_ids()
-            base_pos, orn = self._p.getBasePositionAndOrientation(car_id)
-            view_matrix = self._p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=base_pos,
-                                                                    distance=20.0,
-                                                                    yaw=40.0,
-                                                                    pitch=-35,
-                                                                    roll=0,
-                                                                    upAxisIndex=2)
-            proj_matrix = self._p.computeProjectionMatrixFOV(fov=60,
-                                                             aspect=float(RENDER_WIDTH) / RENDER_HEIGHT,
-                                                             nearVal=0.1,
-                                                             farVal=100.0)
-            (_, _, px, _, _) = self._p.getCameraImage(width=RENDER_WIDTH,
-                                                      height=RENDER_HEIGHT,
-                                                      viewMatrix=view_matrix,
-                                                      projectionMatrix=proj_matrix,
-                                                      renderer=p.ER_BULLET_HARDWARE_OPENGL)
-            frame = np.array(px)
-            frame = frame[:, :, :3]
-            return frame
-        else:
-            return np.array([])
+        # Rendering code
+        pass
 
     def getExtendedObservation(self):
-        # self._observation = []  #self._racecar.getObservation()
-        carpos, carorn = self._p.getBasePositionAndOrientation(self.car.car)
-        goalpos, goalorn = self._p.getBasePositionAndOrientation(self.goal_object.goal)
-        invCarPos, invCarOrn = self._p.invertTransform(carpos, carorn)
-        goalPosInCar, goalOrnInCar = self._p.multiplyTransforms(invCarPos, invCarOrn, goalpos, goalorn)
-
-        observation = [goalPosInCar[0], goalPosInCar[1]]
+        carpos, _ = self._p.getBasePositionAndOrientation(self.car.car)
+        goalpos, _ = self._p.getBasePositionAndOrientation(self.goal_object.goal)
+        observation = np.array([carpos[0] - goalpos[0], carpos[1] - goalpos[1]], dtype=np.float32)
         return observation
 
     def _termination(self):
